7042e6b57665aaa872ff53256fcaba319ca5427c
longtimehorn63 <wade.pellerin@gmail.com>
2018-02-14 13:14:33 -0600

    initial code checkin

---
+++ zipkin-distributed-tracing-server/src/main/java/com/udemy/microservices/ZipkinDistributedTracingServerApplication.java
@@ -0,0 +1,17 @@
+package com.udemy.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+@EnableDiscoveryClient // enabling the discovery for the eureka server
+public class ZipkinDistributedTracingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinDistributedTracingServerApplication.class, args);
+	}
+}
